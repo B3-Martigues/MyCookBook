26f55891db57d3dbbe9bc3ad42814d906a4e4f33
pawel-barc <pawel-barc@laplateforme.io>
2025-02-04 00:41:08 +0100

    feat: The import routs adjusted

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 //Importation du router Express et des contrôleurs nécessaires
 const router = require("express").Router();
 const AuthController = require("../controllers/AuthController");
+const RecipeController = require("../controllers/RecipeController");
+const verifyToken = require("../middlewares/verifyToken");
 //Section dédiée à la définition des routes
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/refresh-token", AuthController.refreshToken);
 router.post("/logout", AuthController.logout);
-
 //Exportation du routeur pour l'utiliser dans le fichier server.js
 module.exports = router;
