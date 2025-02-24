b5e6ec54bbae035f3a8c7625593ea0b16b8165ed
pawel-barc <pawel-barc@laplateforme.io>
2025-02-24 16:59:15 +0100

    clean branch dev for dev v2

--- README.md
+++
@@ -1,2 +0,0 @@
-# CookBook
-A cooking app built with Node.js, MongoDB, React, and Express, allowing users to manage and explore recipes.
--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-//Importation du router Express et des contrôleurs nécessaires
-const router = require("express").Router();
-const AuthController = require("../controllers/AuthController");
-
-//Section dédiée à la définition des routes
-router.post("/register", AuthController.register);
-
-//Exportation du routeur pour l'utiliser dans le fichier server.js
-module.exports = router;
