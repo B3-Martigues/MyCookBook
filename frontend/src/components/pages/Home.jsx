9839735abe1bb573f9a9bb98dea1642ce81cd6e2
pawel-barc <pawel-barc@laplateforme.io>
2025-02-26 15:47:47 +0100

    Title Welcome Home in Home page adjusted

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -3,8 +3,8 @@ import ListRecipes from "../organisms/ListRecipes";
 const Home = () => {
   return (
     <>
-      <ListRecipes />
       <h1>Welcome Home</h1>
+      <ListRecipes />
     </>
   );
 };
