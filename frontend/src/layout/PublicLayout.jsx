e74b17cafbafb7d187fab621713d741eba45493c
Younes Kribeche <younes.kribeche@laplateforme.io>
2025-02-27 11:37:49 +0100

    commit test

--- frontend/src/layout/PublicLayout.jsx
+++ frontend/src/layout/PublicLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import HeaderUnlogged from "../components/organisms/HeaderUnLogged";
+import HeaderUnlogged from "../components/organisms/HeaderUnlogged";
 //Ce composant sert de structure de base pour les pages nécessitant une authentification
 const PublicLayout = () => {
   return (
