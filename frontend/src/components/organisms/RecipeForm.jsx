d740e5bb1d47cbd900ff87736f30d83644c524fd
Younes Kribeche <younes.kribeche@laplateforme.io>
2025-03-31 13:12:09 +0200

    replacing className for recipe-edit-form

--- frontend/src/components/organisms/RecipeForm.jsx
+++ frontend/src/components/organisms/RecipeForm.jsx
@@ -413,7 +413,7 @@ const RecipeForm = ({ recipeId = null, onSuccess, onIngredientChange }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form-my-recipes">
+    <form onSubmit={handleSubmit} className="recipe-edit-form">
       <h1>{isEditing ? "Modifier la recette" : "Ajouter une recette"}</h1>
 
       {/* Affichage des messages d'erreur ou de succès */}
